2e6eef0765d8c73b32d1b8784848a9b48b487ad2
Chaniug <chaniug@vip.qq.com>
2026-05-01 16:35:51 +0800

    fix: 解决CORS问题，添加CORS代理和预置频道源

--- app.js
+++ app.js
@@ -12,6 +12,67 @@ class IPTVCollector {
         this.currentTab = 'collector';
         this.currentCategory = 'all';
         
+        // CORS 代理列表（按优先级排序）
+        this.corsProxies = [
+            'https://api.allorigins.win/raw?url=',
+            'https://corsproxy.io/?',
+            'https://proxy.cors.sh/'
+        ];
+        
+        // 预置频道源（国内常用频道，可直连）
+        this.presetChannels = [
+            { name: 'CCTV-1 综合', url: 'http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/CCTV-1_%E7%BB%BC%E5%90%88.png/200px-CCTV-1_%E7%BB%BC%E5%90%88.png', category: 'cctv' },
+            { name: 'CCTV-2 财经', url: 'http://ivi.bupt.edu.cn/hls/cctv2hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/CCTV-2_%E8%B4%A2%E7%BB%8F.png/200px-CCTV-2_%E8%B4%A2%E7%BB%8F.png', category: 'cctv' },
+            { name: 'CCTV-3 综艺', url: 'http://ivi.bupt.edu.cn/hls/cctv3hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/CCTV-3_%E7%BB%BC%E8%89%BA.png/200px-CCTV-3_%E7%BB%BC%E8%89%BA.png', category: 'cctv' },
+            { name: 'CCTV-4 中文国际', url: 'http://ivi.bupt.edu.cn/hls/cctv4hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/CCTV-4_%E4%B8%AD%E6%96%87%E5%9B%BD%E9%99%85.png/200px-CCTV-4_%E4%B8%AD%E6%96%87%E5%9B%BD%E9%99%85.png', category: 'cctv' },
+            { name: 'CCTV-5+ 体育', url: 'http://ivi.bupt.edu.cn/hls/cctv5hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/CCTV-5_%E8%BF%90%E5%8A%A8.png/200px-CCTV-5_%E8%BF%90%E5%8A%A8.png', category: 'cctv' },
+            { name: 'CCTV-6 电影', url: 'http://ivi.bupt.edu.cn/hls/cctv6hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/CCTV-6_%E7%94%B5%E5%BD%B1.png/200px-CCTV-6_%E7%94%B5%E5%BD%B1.png', category: 'cctv' },
+            { name: 'CCTV-7 国防军事', url: 'http://ivi.bupt.edu.cn/hls/cctv7hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/CCTV-7_%E5%86%9B%E4%BA%8B.png/200px-CCTV-7_%E5%86%9B%E4%BA%8B.png', category: 'cctv' },
+            { name: 'CCTV-8 电视剧', url: 'http://ivi.bupt.edu.cn/hls/cctv8hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/CCTV-8_%E5%89%A7%E5%9C%BA.png/200px-CCTV-8_%E5%89%A7%E5%9C%BA.png', category: 'cctv' },
+            { name: 'CCTV-9 纪录', url: 'http://ivi.bupt.edu.cn/hls/cctv9hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/CCTV-9_%E7%BA%AA%E5%BD%95.png/200px-CCTV-9_%E7%BA%AA%E5%BD%95.png', category: 'cctv' },
+            { name: 'CCTV-10 科教', url: 'http://ivi.bupt.edu.cn/hls/cctv10hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/CCTV-10_%E7%A7%91%E6%95%99.png/200px-CCTV-10_%E7%A7%91%E6%95%99.png', category: 'cctv' },
+            { name: 'CCTV-11 戏曲', url: 'http://ivi.bupt.edu.cn/hls/cctv11.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/CCTV-11_%E6%88%8F%E6%9B%B3.png/200px-CCTV-11_%E6%88%8F%E6%9B%B3.png', category: 'cctv' },
+            { name: 'CCTV-12 社会与法', url: 'http://ivi.bupt.edu.cn/hls/cctv12hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/CCTV-12_%E7%A4%BE%E4%BC%9A%E4%B8%8E%E6%B3%95.png/200px-CCTV-12_%E7%A4%BE%E4%BC%9A%E4%B8%8E%E6%B3%95.png', category: 'cctv' },
+            { name: 'CCTV-13 新闻', url: 'http://ivi.bupt.edu.cn/hls/cctv13.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/CCTV-13_%E6%96%B0%E9%97%BB.png/200px-CCTV-13_%E6%96%B0%E9%97%BB.png', category: 'cctv' },
+            { name: 'CCTV-14 少儿', url: 'http://ivi.bupt.edu.cn/hls/cctv14hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/CCTV-14_%E5%B0%91%E5%84%BF.png/200px-CCTV-14_%E5%B0%91%E5%84%BF.png', category: 'cctv' },
+            { name: 'CCTV-15 音乐', url: 'http://ivi.bupt.edu.cn/hls/cctv15.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/CCTV-15_%E9%9F%B3%E4%B9%90.png/200px-CCTV-15_%E9%9F%B3%E4%B9%90.png', category: 'cctv' },
+            { name: 'CCTV-16 奥林匹克', url: 'http://ivi.bupt.edu.cn/hls/cctv16.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/CCTV-16_%E5%A5%A5%E5%8D%81%E9%93%9C%E9%81%93.png/200px-CCTV-16_%E5%A5%A5%E5%8D%81%E9%93%9C%E9%81%93.png', category: 'cctv' },
+            { name: 'CCTV-17 农业农村', url: 'http://ivi.bupt.edu.cn/hls/cctv17.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/CCTV-17_%E5%86%9C%E4%B8%9A%E5%86%9C%E6%9D%91.png/200px-CCTV-17_%E5%86%9C%E4%B8%9A%E5%86%9C%E6%9D%91.png', category: 'cctv' },
+            { name: 'CETV-1 中国教育', url: 'http://ivi.bupt.edu.cn/hls/cetv1.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/CETV-1_logo.png/200px-CETV-1_logo.png', category: 'other' },
+            { name: '湖南卫视', url: 'http://ivi.bupt.edu.cn/hls/hunanhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/HunanWeishi.png/200px-HunanWeishi.png', category: 'satellite' },
+            { name: '浙江卫视', url: 'http://ivi.bupt.edu.cn/hls/zjhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/ZhejiangTV.png/200px-ZhejiangTV.png', category: 'satellite' },
+            { name: '江苏卫视', url: 'http://ivi.bupt.edu.cn/hls/jshd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Logo_of_Jiangsu_Television.png/200px-Logo_of_Jiangsu_Television.png', category: 'satellite' },
+            { name: '东方卫视', url: 'http://ivi.bupt.edu.cn/hls/dongfanghd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/DragonTV.png/200px-DragonTV.png', category: 'satellite' },
+            { name: '北京卫视', url: 'http://ivi.bupt.edu.cn/hls/btv1hd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/BeijingTV.png/200px-BeijingTV.png', category: 'local' },
+            { name: '上海卫视', url: 'http://ivi.bupt.edu.cn/hls/shtv.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/STV_Logo.png/200px-STV_Logo.png', category: 'local' },
+            { name: '广东卫视', url: 'http://ivi.bupt.edu.cn/hls/gdhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/GuangdongTV.png/200px-GuangdongTV.png', category: 'local' },
+            { name: '深圳卫视', url: 'http://ivi.bupt.edu.cn/hls/szhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/ShenzhenTV.png/200px-ShenzhenTV.png', category: 'local' },
+            { name: '安徽卫视', url: 'http://ivi.bupt.edu.cn/hls/ahhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/AnhuiTV.png/200px-AnhuiTV.png', category: 'local' },
+            { name: '四川卫视', url: 'http://ivi.bupt.edu.cn/hls/schd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/SichuanTV.png/200px-SichuanTV.png', category: 'local' },
+            { name: '山东卫视', url: 'http://ivi.bupt.edu.cn/hls/sdhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/ShandongTV.png/200px-ShandongTV.png', category: 'local' },
+            { name: '湖北卫视', url: 'http://ivi.bupt.edu.cn/hls/hbhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/HubeiTV.png/200px-HubeiTV.png', category: 'local' },
+            { name: '辽宁卫视', url: 'http://ivi.bupt.edu.cn/hls/lnhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/LiaoningTV.png/200px-LiaoningTV.png', category: 'local' },
+            { name: '天津卫视', url: 'http://ivi.bupt.edu.cn/hls/tjhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/TianjinTV.png/200px-TianjinTV.png', category: 'local' },
+            { name: '重庆卫视', url: 'http://ivi.bupt.edu.cn/hls/cqhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/ChongqingTV.png/200px-ChongqingTV.png', category: 'local' },
+            { name: '黑龙江卫视', url: 'http://ivi.bupt.edu.cn/hls/hlhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/HeilongjiangTV.png/200px-HeilongjiangTV.png', category: 'local' },
+            { name: '吉林卫视', url: 'http://ivi.bupt.edu.cn/hls/jlhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/JilinTV.png/200px-JilinTV.png', category: 'local' },
+            { name: '河北卫视', url: 'http://ivi.bupt.edu.cn/hls/hebhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/HebeiTV.png/200px-HebeiTV.png', category: 'local' },
+            { name: '云南卫视', url: 'http://ivi.bupt.edu.cn/hls/yntv.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/YunnanTV.png/200px-YunnanTV.png', category: 'local' },
+            { name: '贵州卫视', url: 'http://ivi.bupt.edu.cn/hls/gztv.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/GuizhouTV.png/200px-GuizhouTV.png', category: 'local' },
+            { name: '海南卫视', url: 'http://ivi.bupt.edu.cn/hls/hainan.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/HainanTV.png/200px-HainanTV.png', category: 'local' },
+            { name: '江西卫视', url: 'http://ivi.bupt.edu.cn/hls/jxhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/JiangxiTV.png/200px-JiangxiTV.png', category: 'local' },
+            { name: '山西卫视', url: 'http://ivi.bupt.edu.cn/hls/shxhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/ShanxiTV.png/200px-ShanxiTV.png', category: 'local' },
+            { name: '内蒙古卫视', url: 'http://ivi.bupt.edu.cn/hls/nmhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/InnerMongoliaTV.png/200px-InnerMongoliaTV.png', category: 'local' },
+            { name: '广西卫视', url: 'http://ivi.bupt.edu.cn/hls/gxhd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/GuangxiTV.png/200px-GuangxiTV.png', category: 'local' },
+            { name: '甘肃卫视', url: 'http://ivi.bupt.edu.cn/hls/gshd.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/GansuTV.png/200px-GansuTV.png', category: 'local' },
+            { name: '宁夏卫视', url: 'http://ivi.bupt.edu.cn/hls/nxtv.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/NingxiaTV.png/200px-NingxiaTV.png', category: 'local' },
+            { name: '青海卫视', url: 'http://ivi.bupt.edu.cn/hls/qhtv.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/QinghaiTV.png/200px-QinghaiTV.png', category: 'local' },
+            { name: '新疆卫视', url: 'http://ivi.bupt.edu.cn/hls/xjtv.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/XinjiangTV.png/200px-XinjiangTV.png', category: 'local' },
+            { name: '西藏卫视', url: 'http://ivi.bupt.edu.cn/hls/xztv.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/TibetTV.png/200px-TibetTV.png', category: 'local' },
+            { name: '凤凰卫视中文', url: 'http://ivi.bupt.edu.cn/hls/phoenix.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/PhoenixTV.png/200px-PhoenixTV.png', category: 'satellite' },
+            { name: '凤凰卫视资讯', url: 'http://ivi.bupt.edu.cn/hls/phoenixinfo.m3u8', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/PhoenixTV.png/200px-PhoenixTV.png', category: 'satellite' }
+        ];
+        
         this.init();
     }
 
@@ -21,6 +82,25 @@ class IPTVCollector {
         this.loadSettings();
         this.renderFavorites();
         this.updateStats();
+        
+        // 自动加载预置频道
+        this.loadPresetChannels();
+    }
+    
+    // 加载预置频道
+    loadPresetChannels() {
+        if (this.sources.length === 0) {
+            this.sources = this.presetChannels.map(ch => ({
+                name: ch.name,
+                url: ch.url,
+                logo: ch.logo || '',
+                category: ch.category,
+                status: 'pending',
+                tested: false
+            }));
+            this.renderSourceList();
+            this.updateStats();
+        }
     }
 
     // 绑定事件
@@ -143,55 +223,74 @@ class IPTVCollector {
                 try {
                     const channels = await this.fetchChannels(url);
                     allChannels.push(...channels);
+                    this.showToast(`从 ${new URL(url).hostname} 获取到 ${channels.length} 个频道`, 'success');
                 } catch (e) {
                     console.warn(`从 ${url} 采集失败:`, e);
+                    this.showToast(`从 ${url} 采集失败，使用预置频道`, 'error');
                 }
             }
             
             // 去重
             const uniqueChannels = this.deduplicateChannels(allChannels);
             
+            // 合并预置频道（避免重复）
+            const presetUrls = new Set(this.presetChannels.map(ch => ch.url));
+            const newChannels = uniqueChannels.filter(ch => !presetUrls.has(ch.url));
+            
             // 分配类别
-            this.sources = uniqueChannels.map(ch => ({
-                ...ch,
-                category: this.categorizeChannel(ch.name),
-                status: 'pending',
-                tested: false
-            }));
+            this.sources = [
+                ...this.presetChannels.map(ch => ({
+                    name: ch.name,
+                    url: ch.url,
+                    logo: ch.logo || '',
+                    category: ch.category,
+                    status: 'pending',
+                    tested: false
+                })),
+                ...newChannels.map(ch => ({
+                    ...ch,
+                    category: this.categorizeChannel(ch.name),
+                    status: 'pending',
+                    tested: false
+                }))
+            ];
             
             this.renderSourceList();
             this.updateStats();
-            this.showToast(`成功采集 ${this.sources.length} 个频道`, 'success');
+            this.showToast(`共 ${this.sources.length} 个频道（预置 ${this.presetChannels.length} + 采集 ${newChannels.length}）`, 'success');
             
             // 自动测试
             setTimeout(() => this.testAllSources(), 500);
             
         } catch (error) {
             this.showToast('采集失败: ' + error.message, 'error');
+            // 确保预置频道可用
+            if (this.sources.length === 0) {
+                this.loadPresetChannels();
+                this.showToast('已加载预置频道，请点击"测试全部"', 'error');
+            }
         } finally {
             this.hideLoading();
         }
     }
 
-    // 获取频道数据
+    // 获取频道数据（通过 CORS 代理）
     async fetchChannels(url) {
+        const data = await this.fetchWithProxy(url);
+        
         if (url.includes('iptv-org')) {
-            const response = await fetch(url);
-            const data = await response.json();
-            
-            return data.map(item => ({
-                name: item.name,
-                url: item.url,
-                logo: item.logo || '',
-                country: item.country?.[0] || '',
-                category: item.category || []
-            }));
+            if (Array.isArray(data)) {
+                return data.map(item => ({
+                    name: item.name,
+                    url: item.url,
+                    logo: item.logo || '',
+                    country: item.country?.[0] || '',
+                    category: item.category || []
+                }));
+            }
         }
         
-        // 通用 fetch
-        const response = await fetch(url);
-        const data = await response.json();
-        
+        // 通用解析
         if (Array.isArray(data)) {
             return data.map(item => ({
                 name: item.name || item.title || '未知频道',
@@ -203,6 +302,37 @@ class IPTVCollector {
         }
         
         return [];
+    }
+    
+    // 使用 CORS 代理获取数据
+    async fetchWithProxy(url) {
+        // 尝试使用多个代理
+        for (const proxy of this.corsProxies) {
+            try {
+                const proxyUrl = proxy + encodeURIComponent(url);
+                const response = await fetch(proxyUrl, {
+                    timeout: 15000
+                });
+                
+                if (response.ok) {
+                    return await response.json();
+                }
+            } catch (e) {
+                console.warn(`代理 ${proxy} 失败:`, e);
+            }
+        }
+        
+        // 所有代理都失败，尝试直接获取（可能失败，但给出友好错误）
+        try {
+            const response = await fetch(url);
+            if (response.ok) {
+                return await response.json();
+            }
+        } catch (e) {
+            console.warn('直接请求失败:', e);
+        }
+        
+        throw new Error('无法获取数据，请检查网络或稍后重试');
     }
 
     // 去重
